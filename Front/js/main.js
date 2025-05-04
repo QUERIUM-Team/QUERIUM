758b27f39e53372c734b7a3284c51b893caf9e3e
Ahmed Allam <60698204+AllamF5J@users.noreply.github.com>
2025-05-04 17:46:05 +0300

    Add A Structure Of Website And A Home Page With AOS, Animations and Js Code Of Filtering

---
+++ Front/js/main.js
@@ -0,0 +1,52 @@
+/** @format */
+
+// ! Make a PopUp To A Lastest News Photos.
+let img = document.querySelectorAll(".news .data .item img");
+img.forEach((ele) => {
+  ele.addEventListener("click", (e) => {
+    let div = document.createElement("div");
+    div.className = "popup-overlay";
+
+    let popUp = document.createElement("div");
+    popUp.className = "popUp";
+
+    let popUpImg = document.createElement("img");
+    popUpImg.src = ele.src;
+
+    div.addEventListener("click", function () {
+      popUp.remove();
+      div.remove();
+    });
+
+    document.body.appendChild(popUp);
+    document.body.appendChild(div);
+    popUp.appendChild(popUpImg);
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Initialize Isotope
+  const portfolioIsotope = new Isotope(".files-content", {
+    itemSelector: ".mega",
+    layoutMode: "fitRows", // Ensure the items are rearranged in rows
+  });
+
+  // Get all buttons
+  const buttons = document.querySelectorAll(".main-content .buttons button");
+
+  // Add event listener for button clicks
+  buttons.forEach((button) => {
+    button.addEventListener("click", function () {
+      // Remove 'active' class from all buttons
+      buttons.forEach((btn) => btn.classList.remove("active"));
+
+      // Add 'active' class to clicked button
+      this.classList.add("active");
+
+      // Apply filter to the isotope layout
+      portfolioIsotope.arrange({
+        filter: this.getAttribute("data-filter"),
+      });
+    });
+  });
+});
