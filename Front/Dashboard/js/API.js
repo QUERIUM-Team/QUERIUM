c3a61705826fe69aa2bc1dd38c23a0506da48087
Ahmed Allam <60698204+AllamF5J@users.noreply.github.com>
2025-05-13 04:23:41 +0300

    Make A API Auth Of Dashboard Logout

--- Front/Dashboard/js/API.js
+++ Front/Dashboard/js/API.js
@@ -1,5 +1,6 @@
 /** @format */
 
+// ! Login Function.
 async function login(event) {
   event.preventDefault(); // Prevent the default form submission
 
@@ -30,5 +31,40 @@ async function login(event) {
     console.error("Error:", error);
     document.getElementById("error").textContent =
       "An error occurred. Please try again.";
+  }
+}
+
+// ! Logout Function.
+async function logout() {
+  const messageDiv = document.getElementById("logout-message");
+  messageDiv.className = "logout-message"; // reset styles
+  messageDiv.textContent = ""; // clear old message
+
+  try {
+    const response = await fetch(
+      "http://querium13.runasp.net/api/Admin/logout",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const data = await response.json();
+
+    messageDiv.textContent = data.message;
+
+    if (data.message === "Admin logout successful") {
+      messageDiv.classList.add("success");
+      setTimeout(() => {
+        window.location.href = "log-out.html";
+      }, 200); // redirect after 2s
+    } else {
+      messageDiv.classList.add("error");
+    }
+  } catch (error) {
+    messageDiv.textContent = "An error occurred during logout.";
+    messageDiv.classList.add("error");
   }
 }
